fe77fed0da5768cf7d452e9c304bc338f1e32355
Maicon Machado <147193871+maiconarroyo@users.noreply.github.com>
2024-10-11 12:44:12 -0300

    refeito o design da calculadora de amônia , alterado js , retirado barra lateral no mobile , ajustado header no mobile

--- javaScript/teste.js
+++ javaScript/teste.js
@@ -1,9 +1,20 @@
+function hidem() {
+    const botomCalculator = document.querySelector('.icon-calculator');
+    const aboutDiv = document.querySelector('.about-amonia');
+    const calcularDiv = document.querySelector('.calcular-amonia');
+    aboutDiv.style.display = 'none';
+    calcularDiv.style.display = 'flex';
+    botomCalculator.style.display = 'none';
+}
+
 function amonia() {
-    alert("Em Desenvolvimento");
-    
-    let temperatura = parseFloat(prompt("Qual a Temperatura (°C)?"));
-    let ph = parseFloat(prompt("Qual o pH?"));
-    let amoniaTotal = parseFloat(prompt("Qual valor do Teste de Amônia (mg/L)?"));
+    const amoniaInput = document.querySelector('input[name="amonia"]');
+    const phInput = document.querySelector('input[name="ph"]');
+    const temperaturaInput = document.querySelector('input[name="temperatura"]');
+
+    let amoniaTotal = parseFloat(amoniaInput.value);
+    let ph = parseFloat(phInput.value);
+    let temperatura = parseFloat(temperaturaInput.value);
 
     if (isNaN(temperatura) || isNaN(ph) || isNaN(amoniaTotal)) {
         alert("Por favor, insira valores válidos.");
@@ -19,17 +30,20 @@ function amonia() {
     const amoniaToxica = amoniaTotal * fracaoToxica;
 
     const resultadoDiv = document.getElementById('resultado');
+    const resultadoTexto = resultadoDiv.querySelector('.sub-title.-resultado');
     resultadoDiv.style.display = 'block';
 
     if (amoniaToxica <= 0.001) {
         resultadoDiv.style.backgroundColor = 'green';
-        resultadoDiv.textContent = "Níveis de Amônia Seguros";
+        resultadoTexto.style.color = 'white';
+        resultadoTexto.textContent = "Níveis de Amônia Seguros";
     } else if (amoniaToxica <= 0.086) {
         resultadoDiv.style.backgroundColor = 'yellow';
-        resultadoDiv.style.color = 'Black'; 
-        resultadoDiv.textContent = "Cuidado! Níveis de Amônia Moderado";
+        resultadoTexto.style.color = 'black';
+        resultadoTexto.textContent = "Cuidado! Níveis de Amônia Moderado";
     } else {
         resultadoDiv.style.backgroundColor = 'red';
-        resultadoDiv.textContent = "Níveis de Amônia Críticos!!";
+        resultadoTexto.style.color = 'white';
+        resultadoTexto.textContent = "Níveis de Amônia Críticos!!";
     }
 }
